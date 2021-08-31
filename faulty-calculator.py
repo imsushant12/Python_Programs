8c00484361584c65567fb61ccbe9d2002ccfa370
imsushant12 <sushantgaurav57@gmail.com>
2021-08-31 10:58:48 +0530

    first commit

--- faulty-calculator.py
+++ faulty-calculator.py
@@ -2,13 +2,16 @@ operator = input("Enter the operator: ")
 num1 = int(input("Enter number 1: "))
 num2 = int(input("Enter number 2: "))
 
-if operator == "+":
-    print(num1+num2)
-elif operator == "-":
-    print(num1-num2)
-elif operator == "*":
-    print(num1*num2)
-elif operator == "/":
-    print(num1/num2)
-else:
+try:
+    if operator == "+":
+        print(num1+num2)
+    elif operator == "-":
+        print(num1-num2)
+    elif operator == "*":
+        print(num1*num2)
+    elif operator == "/":
+        print(num1/num2)
+    elif operator == "**":
+        print(num1**num2)
+except:
     print("Try another operator")
