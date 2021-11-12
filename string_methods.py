fbdb8f364701ca7421902bc6042f7951c4a830a7
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-11-12 23:07:41 +0530

    first commit

---
+++ string_methods.py
@@ -0,0 +1,35 @@
+""""
+10 methods of String. 
+"""
+
+s = "hello world!"
+
+# 1. isupper()
+print(s.isupper())
+
+# 2. islower()
+print(s.islower())
+
+# 3. replace()
+print(s.replace("W", "w"))
+
+# 4.split(character)
+print(s.split("o"))
+
+# 5.casefold()
+print(s.casefold())
+
+# 6. encode()
+print(s.encode())
+
+# 7. find(character)
+print(s.find("h"))
+
+# 8. isalpha()
+print(s.isalpha())
+
+# 9. title()
+print(s.title())
+
+# 10. reversing a sting: 
+print(s[::-1])
