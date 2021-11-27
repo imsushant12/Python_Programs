df03c2797f648fa9702c94fa014a4a746bb3ad11
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-11-27 21:01:39 +0530

    first commit

--- test.py
+++ test.py
@@ -1,26 +1,21 @@
-import pygame
-import math
-import sys
+def stringShift(self, s: str, shift: List[List[int]]) -> str:
+    l, ls, rs = len(s), 0, 0
+    for direction, amount in shift:
+        if direction == 0:
+            ls += amount
+        else:
+            rs += amount
+    ls = ls % l
+    rs = rs % l
+    if ls == rs:
+        return s
+    elif ls > rs:
+        ls = ls - rs
+        return s[ls:]+s[:ls]
+    else:
+        rs = rs - ls
+        return s[-rs:]+s[:-rs]
 
-pygame.init()
+shift = [[0,1],[1,2]]
 
-screen = pygame.display.set_mode((600, 300)) 
-pygame.display.set_caption("Elliptical orbit")
-clock=pygame.time.Clock() 
-
-while(True):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()   
-
-    xRadius = 250
-    yRadius = 100
-    for degree in range(0,360,10):
-        x1 = int(math.cos(degree * 2 * math.pi/360) * xRadius)+300
-        y1 = int(math.sin(degree * 2 * math.pi/360) * yRadius)+150
-        screen.fill((0, 0, 0))
-        pygame.draw.circle(screen, (255, 69, 0), [300, 150], 40)
-        pygame.draw.ellipse(screen,(255,255,255),[50,50,500,200],1)
-        pygame.draw.circle(screen, (0, 255, 0), [x1, y1], 20)
-        pygame.display.flip()
-        clock.tick(5)
+stringShift("abc", shift)
